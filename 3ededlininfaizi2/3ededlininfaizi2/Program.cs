bbc9b55396e6a01f394705688a2084f2df449d56
ashraf032 <101041982+ashraf032@users.noreply.github.com>
2022-03-25 19:09:46 +0400

    result

---
+++ 3ededlininfaizi2/3ededlininfaizi2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _3ededlininfaizi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sevenpersentage = "7";
+
+            Console.WriteLine("-------------------------");
+            
+            Console.WriteLine("Zehmet olmasa 3 reqemli eded daxil edin");
+            
+            string num = Console.ReadLine();
+            
+            string couplestr = num + sevenpersentage;
+            
+            Console.WriteLine(couplestr);
+            
+            int resultparam = int.Parse(couplestr);
+            
+            if (resultparam>=100 && resultparam<10000)
+            {
+                Console.WriteLine(resultparam*0.07);
+            }
+            
+            else
+            {
+                Console.WriteLine("Nezere alinmayib");
+            }
+
+
+        }
+
+    }
+}
